e0220a9a5f2dc391f70cf966df624ecfb649f51a
philliphnguyen <phillip.nguyen1997@yahoo.com>
2023-11-30 15:13:56 -0800

    changes to vaccination card css

--- frontend/src/components/VaccinationCard.js
+++ frontend/src/components/VaccinationCard.js
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import QRCode from 'qrcode.react';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import QRCode from "qrcode.react";
+import CheckIcon from "@mui/icons-material/Check";
 import { API_URL } from "../apiConfig";
+import { Container, Divider } from "@material-ui/core";
 
 const VaccinationCard = () => {
   const { uniqueId } = useParams();
 
-  console.log(uniqueId)
+  console.log(uniqueId);
   // const uniqueId = "9999"
   const [vaccinationRecord, setVaccinationRecord] = useState(null);
 
@@ -28,30 +29,62 @@ const VaccinationCard = () => {
   }
 
   return (
-    <Card sx={{ maxWidth: 345, background: red[500], color: '#fff', borderRadius: 2 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="textSecondary" gutterBottom>
-          Vaccination Card - COVID-19
-        </Typography>
-        <Typography variant="h5" component="h2">
-          {vaccinationRecord.name}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }}>
-          Vaccine: {vaccinationRecord.typeOfVaccine}
-        </Typography>
-        <Typography variant="body2">
-          First Dose: {vaccinationRecord.dateOfFirstDose} <br />
-          Second Dose: {vaccinationRecord.dateOfSecondDose} <br />
-          {/* Issued by: {vaccinationRecord.typeOfVaccine} */}
-        </Typography>
-        <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0' }}>
-          <QRCode value={`${window.location.origin}/vaccinationCard/${uniqueId}`} size={128} level={"H"} includeMargin={true} />
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-          <FavoriteIcon sx={{ color: '#fff' }} />
-        </div>
-      </CardContent>
-    </Card>
+    <Container style={{ display: "flex", justifyContent: "center" }}>
+      <Card
+        sx={{
+          minWidth: 325,
+          background: red[500],
+          color: "#fff",
+          borderRadius: 2,
+          marginTop: "20px",
+        }}
+      >
+        <CardContent sx={{ padding: "16px 0px !important" }}>
+          <Typography sx={{ fontSize: 14, marginBottom: "10px" }} gutterBottom>
+            Vaccination Card - COVID-19
+          </Typography>
+          <Divider
+            sx={{
+              backgroundColor: "#fff !important",
+            }}
+          />
+          <Typography variant="h5" component="h2" sx={{ marginTop: "20px" }}>
+            {vaccinationRecord.name}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }}>
+            Vaccine: {vaccinationRecord.typeOfVaccine}
+          </Typography>
+          <Typography variant="body2">
+            First Dose: {vaccinationRecord.dateOfFirstDose} <br />
+            Second Dose: {vaccinationRecord.dateOfSecondDose} <br />
+            {/* Issued by: {vaccinationRecord.typeOfVaccine} */}
+          </Typography>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "30px",
+            }}
+          >
+            <QRCode
+              value={`${window.location.origin}/vaccinationCard/${uniqueId}`}
+              size={175}
+              level={"H"}
+              includeMargin={true}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "10px",
+            }}
+          >
+            <CheckIcon sx={{ color: "#fff" }} />
+          </div>
+        </CardContent>
+      </Card>
+    </Container>
   );
 };
 
